b7a9f0d134219aca523be6138a3e23f38ca3f77b
Will Lehman <william.p.lehman@usace.army.mil>
2022-03-13 01:03:06 +0000

    adding a convergence criteria

---
+++ component/convergencecriteria.go
@@ -0,0 +1,9 @@
+package component
+
+//this should probably live in go-statistics with inline histogram.
+type ConvergenceCriteria struct {
+	MinIterations int64   `json:"min_iterations"`
+	MaxIterations int64   `json:"max_iterations"`
+	ZAlpha        float64 `json:"z_alpha"`
+	Tolerance     float64 `json:"tolerance"`
+}
